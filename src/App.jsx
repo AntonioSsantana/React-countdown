22966eabc6092ef77716c8b8772efe5172b45267
Antônio Santana <antonionetosg6@gmail.com>
2023-05-28 13:57:41 -0300

    feat: register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
+// Components
+import { Register } from "./pages";
+
 function App() {
   return (
    <Routes>
     <Route exact path="/" element={<Navigate to={"/login"}/>} />
+    <Route exact path="/register" element={<Register />} />
    </Routes>
   )
 }
