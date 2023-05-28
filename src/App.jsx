9c73e261738208f7a400d70ef28d1299d3605af1
Antônio Santana <antonionetosg6@gmail.com>
2023-05-28 16:48:27 -0300

    feat: implementa a rota e elementos necessários

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 // Components
-import { Login } from "./pages/index";
-import { Register } from "./pages";
+import { Login, Register, Home } from "./pages/index";
 
 function App() {
   return (
-   <Routes>
-    <Route exact path="/" element={<Navigate to={"/login"}/>} />
-    <Route exact path="/login" element={<Login />} />
-    <Route exact path="/register" element={<Register />} />
-   </Routes>
+    <Routes>
+      <Route exact path="/" element={<Navigate to={"/login"} />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/home" element={<Home />} />
+    </Routes>
   )
 }
 
